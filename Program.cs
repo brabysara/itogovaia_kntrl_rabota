fa9732c7cd333a837bf8587df523b7f0301165e3
brabysara <alchakov.ilia@gmail.com>
2023-08-01 19:37:49 +0300

    Готово

--- Program.cs
+++ Program.cs
@@ -7,19 +7,20 @@
 Console.WriteLine("Впишите любое количество символов");
 string vvod = Console.ReadLine();
 string[] all_mas = new string []{"idsrew","-23","546345",vvod};
-string[] new_mas = new string [4];
+string[] new_mas = new string [all_mas.Length];
 int zamen = 0 ;
 //Проверка и перенос данных масисва 
 for (int z = 0; z < all_mas.Length; z++)
 {
     if(all_mas[z].Length <= 3)
     {
-        Console.WriteLine(all_mas[z]);
-
         new_mas[zamen] = all_mas[z];
         zamen++;
+        Console.WriteLine(new_mas[zamen]); 
     } 
+    
 }
+Console.Write(new_mas[zamen]); 
 
 // Вывод первоначальных данных в консоль
 Console.Write("[" ); 
@@ -27,14 +28,11 @@ for (int i = 0; i < all_mas.Length; i++)
 {
    Console.Write("'"+ all_mas[i]+"'"+", "); 
 }
-Console.Write("] -> [" ); 
+Console.Write("] -> [  " ); 
  
 // Вывод проверенных данных в консоль
-for (int gg = 0; gg < new_mas[gg].Length; gg++)
-{
-    if( new_mas[gg].Length >= 1 )
-    {
-    Console.Write("'"+ new_mas[gg]+"' "); 
-    }
+for (int gg = 0; gg < new_mas.Length; gg++)
+{   
+    Console.Write(new_mas[gg]+" "); 
 }
 Console.Write("]" );
